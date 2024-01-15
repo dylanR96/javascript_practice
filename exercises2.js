b0dea639b35b196ee0b8453c26ff42292b3c4449
dylanR96 <dylan.ravenson@gmail.com>
2024-01-15 13:05:53 +0100

    Completed javascript array exercises

--- exercises2.js
+++ exercises2.js
@@ -42,3 +42,43 @@ console.log(fruits);
 
 fruits.shift();
 console.log(fruits);
+
+fruits.splice(1, 0, "orange");
+console.log(fruits);
+
+fruits.splice(3, 0, "Dragon fruit, banana, berry");
+console.log(fruits);
+
+let names = ['David', 'Christoffer', 'Johan', 'Maja']
+names.splice(1,2);
+console.log(names);
+
+let nums = [1,2,3,4,5,6,7,8,9];
+console.log(nums);
+let numsReversed = nums.reverse();
+console.log(numsReversed);
+
+let str = 'Supercalifragilisticexpialidocious';
+if(str.includes("n")) {
+    console.log("The array contains n!");
+}
+
+if(str.includes("x")){
+    console.log("The array contains x!");
+}
+
+if(str.includes("p")){
+    console.log(str.indexOf("p"))
+}
+
+for(let i = 0; i <= 5; i++) {
+    console.log(str[i]);
+} 
+
+let strArr = str.split('');
+let length = strArr.length;
+for(let i = length; i > length - 8; i--) {
+    console.log(strArr[i]);
+}
+
+
